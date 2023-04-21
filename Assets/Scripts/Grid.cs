1ee4dc433228f619b6e9452e73af89b2d73ce14d
iumyx2612 <deftnottroanh@gmail.com>
2023-04-22 00:51:57 +0700

    Update A* Path Fiding

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -9,10 +9,13 @@ public class Grid : MonoBehaviour
     [SerializeField] private Vector2 gridWorldSize; // Area in world coord that grid is going to cover
     [SerializeField] private float nodeRadius; // How much individual node covers
     private Node[,] grid; // Matrix of nodes
+    public List<Node> paths;
     
     // Variables to calculate how many nodes can fit in the grid area
     private float nodeDiameter; // Calculated diameter for ONE node
     private int numNodeX, numNodeY; // Number of nodes to fit in axis
+
+    public bool onlyDisplayPath;
 
     private void Start()
     {
@@ -27,19 +30,78 @@ public class Grid : MonoBehaviour
     {
         Gizmos.DrawWireCube(transform.position, new Vector2(
             gridWorldSize.x, gridWorldSize.y));
-        if (grid != null)
+        if (onlyDisplayPath)
         {
-            foreach (Node node in grid)
+            if (paths != null)
             {
-                // If Node is obstacle, color with red, else white
-                Gizmos.color = (node.isObstacle) ? Color.white : Color.red;
-                Gizmos.DrawCube(node.nodeCenterPos, Vector3.one * (nodeDiameter - 0.05f));
+                foreach (Node node in paths)
+                {
+                    Gizmos.color = new Color(0, 1, 0, 0.5f);
+                    Gizmos.DrawCube(node.nodeCenterPos, Vector3.one * (nodeDiameter - 0.05f));
+                }
             }
         }
+        else
+        {
+            if (grid != null)
+            {
+                foreach (Node node in grid)
+                {
+                    // If Node is obstacle, color with red, else white
+                    if (node.isObstacle)
+                        Gizmos.color = new Color(1, 0, 0, 0.5f);
+                    else
+                        Gizmos.color = new Color(1, 1, 1, 0.5f);
+                    if (paths != null)
+                    {
+                        if (paths.Contains(node))
+                        {
+                            Gizmos.color = new Color(0, 1, 0, 0.5f);
+                        }
+                    }
+                    Gizmos.DrawCube(node.nodeCenterPos, Vector3.one * (nodeDiameter - 0.05f));
+                }
+            }   
+        }
+
     }
 
+    public int MaxSize
+    {
+        get { return numNodeX * numNodeY; }
+    }
+
+    public List<Node> GetNeighbors(Node parent)
+    {
+        List<Node> neighbors = new List<Node>();
+        
+        // Search through 3x3 kernel, parent is [0, 0] 
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int y = -1; y <= 1; y++)
+            {
+                if (x == 0 && y == 0)
+                {
+                    continue;
+                }
+                // Get the current Node in the kernel region
+                int curX = parent.gridX + x;
+                int curY = parent.gridY + y;
+                
+                // Check if current Node is inside the grid
+                // Must be greater or equal to 0 and smaller than the number of Node on axis
+                if (curX >= 0 && curX < numNodeX && curY >= 0 && curY < numNodeY)
+                {
+                    neighbors.Add(grid[curX, curY]);
+                }
+            }
+        }
+
+        return neighbors;
+    }
+    
     // This function returns a node from world position
-    private Node NodeFromWorldPos(Vector2 worldPos)
+    public Node NodeFromWorldPos(Vector2 worldPos)
     {
         // We need to add (gridWorldSize / 2) because worldPos.x can be negative
         float percentX = (worldPos.x + gridWorldSize.x / 2) / gridWorldSize.x;
@@ -72,8 +134,8 @@ public class Grid : MonoBehaviour
                                      Vector2.up * (y * nodeDiameter + nodeRadius);
                 // Check if that node is an obstacle
                 // If there's collider means there's obstacle
-                bool isObstacle = !Physics2D.OverlapCircle(nodeCenter, nodeRadius, obstacleMask);
-                grid[x, y] = new Node(isObstacle, nodeCenter);
+                bool isObstacle = Physics2D.OverlapCircle(nodeCenter, nodeRadius, obstacleMask);
+                grid[x, y] = new Node(isObstacle, nodeCenter, x, y);
             }
         }
     }
--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -2,14 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Node
+public class Node : IHeapItem<Node>
 {
     public bool isObstacle; // If the node is an obstacle
     public Vector3 nodeCenterPos; // Center of the Node in World Point coord
+    
+    // Keep track of where the Node is in the grid
+    public int gridX; 
+    public int gridY;
+    
+    public int gCost; // Cost from starting to end node
+    public int hCost; // Cost from end to starting node
 
-    public Node(bool _isObstacle, Vector3 _nodeCenterPos)
+    public Node parent;
+
+    private int heapIndex;
+
+    public Node(bool _isObstacle, Vector3 _nodeCenterPos, int _gridX, int _gridY)
     {
         isObstacle = _isObstacle;
         nodeCenterPos = _nodeCenterPos;
+        gridX = _gridX;
+        gridY = _gridY;
+    }
+
+    public int fCost
+    {
+        get { return gCost + hCost; }
+    }
+
+    public int HeapIndex
+    {
+        get { return heapIndex; }
+        set { heapIndex = value; }
+    }
+
+    public int CompareTo(Node nodeToCompare)
+    {
+        int compare = fCost.CompareTo(nodeToCompare.fCost);
+        if (compare == 0)
+        {
+            compare = hCost.CompareTo(nodeToCompare.hCost);
+        }
+
+        return -compare;
     }
 }
